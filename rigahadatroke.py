a5b799bb0803f579416cfaa4a1a0968b9d3bec67
yuki <35743654+yukij8@users.noreply.github.com>
2020-06-18 17:09:28 +0200

    Added main file

---
+++ rigahadatroke.py
@@ -0,0 +1,9 @@
+import random
+
+abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+sent = input().lower()
+
+sent = random.choice(abc).join(sent[i:i + random.randint(1, 9)] for i in range(0, len(sent), random.randint(1, 9)))
+print(sent)
+
+# WRITTEN BY YUKI, ITS FUCKING SHIT BUT EH IT WORKS, REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
